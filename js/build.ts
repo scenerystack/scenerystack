3bf76f4aeff13df9098d824ee3a42031b7eb8fff
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-19 15:41:34 -0600

    fixing scenerystack using QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/45

--- js/build.ts
+++ js/build.ts
@@ -416,7 +416,8 @@ export default localeData;` );
         {
           // TODO: remove this
           if ( repo !== 'sherpa' ) {
-            if ( !destPath.includes( 'QueryStringMachine' ) && !destPath.includes( `assert${path.sep}js${path.sep}assert` ) && modifiedContent.includes( 'QueryStringMachine' ) ) {
+            if ( !destPath.includes( 'QueryStringMachine' ) && !destPath.includes( `assert${path.sep}js${path.sep}assert` ) &&
+                 modifiedContent.includes( 'QueryStringMachine' ) && !modifiedContent.includes( 'QueryStringMachineModule' ) ) {
               insertImport( `import '${getImportPath( 'src/query-string-machine/js/QueryStringMachine.js' )}';` );
             }
             if ( !destPath.includes( `src${path.sep}assert` ) && modifiedContent.includes( 'assert' ) ) {
